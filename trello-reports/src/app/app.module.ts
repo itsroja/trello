be633c633a764ab59608fa860ad75aea4184f0b3
David Piper <dbpiper@gmail.com>
2017-04-04 13:35:01 -0500

    Add imports for ng2-charts.

--- trello-reports/src/app/app.module.ts
+++ trello-reports/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ChartsModule } from 'ng2-charts';
 
 import { TrelloService } from './services/trello.service';
 import { HttpService } from './services/http.service';
@@ -23,7 +24,8 @@ import { BoardComponent } from './board/board.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartsModule
   ],
   providers: [
     DataService,
