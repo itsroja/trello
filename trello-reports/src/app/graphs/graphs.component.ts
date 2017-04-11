62b9f1990bf0e67e7697b32f3465b85a4e06a4c0
Ahmed Boulad <ahmedboulad@yahoo.com>
2017-04-10 20:18:34 -0500

    Added functionality to bar chart in graphs component

--- trello-reports/src/app/graphs/graphs.component.ts
+++ trello-reports/src/app/graphs/graphs.component.ts
@@ -13,10 +13,25 @@ export class GraphsComponent {
     boardID: string = 'rvPpS2c8';
     members: string[];
     totalStoryPoints: number;
-    memberStoryPoints: number[];
+    memberStoryPoints: number[] = [];
+    sprint: string = "1";
+    currentPointCount: number = 0;
+    sprintLists = [];
+    tempMemberPoints: number[] = [];
+    pieReady: boolean = false;
+    chartType: string = "pie";
 
     constructor(public trelloService: TrelloService, public ref: ChangeDetectorRef){
         this.getData();
+        this.getLists();
+    }
+
+    refresh(){
+        this.pieReady = true;
+    }
+
+    changeType(){
+        this.chartType = (this.chartType == "pie") ? "bar" : "pie";
     }
 
     //******************************************Pie Chart*********************************************//
@@ -26,7 +41,7 @@ export class GraphsComponent {
     };
 
     public pieChartLabels:string[] = [];
-    public pieChartData:number[] = [9, 3, 4, 8, 6, 1, 5, 4];
+    public pieChartData:number[] = [];//[9, 3, 4, 8, 6, 1, 5, 4];
     public pieChartType:string = 'pie';
 
     // events
@@ -47,14 +62,14 @@ export class GraphsComponent {
         maintainAspectRatio: false
     };
 
-    public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    public barChartLabels:string[] = [];//['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
     public barChartType:string = 'bar';
     public barChartLegend:boolean = true;
 
-    public barChartData:any[] = [
+    public barChartData:any[] = [{data: [], label: 'Story Points Completed'}];/*[
         {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
         {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-    ];
+    ];*/
     //*******************************************************************************************//
 
     getData(){
@@ -66,21 +81,67 @@ export class GraphsComponent {
         var m = [];
         for(let member of members){
             m.push(member.fullName);
+            this.pieChartData.push(0);
         }
         this.pieChartLabels = m;
         this.ref.markForCheck();
-        //this.getCards(members);
     }
 
-    getCards(members){
-        for(let member of members){
-            this.trelloService.trello.get('/boards/' + this.boardID + '/members/' + member.id + '/cards', (cards) => this.printCardNames(cards));
+    getLists(){
+        this.trelloService.trello.get('/boards/' + this.boardID + '/lists?cards=all', (lists) => this.setLists(lists));
+    }
+
+    setLists(lists){
+        for(let list of lists){
+            if(list.name.includes("Sprint " + this.sprint) && !list.name.includes("Complete")){
+                this.sprintLists.push(list);
+            }
+        }
+
+        for(let list of this.sprintLists){
+            this.trelloService.trello.get('/list/' + list.id + '/cards', (cards) => this.filterCards(cards));
+        }
+        this.bargraphLists(lists);
+        this.ref.markForCheck();
+    }
+
+    filterCards(cards){
+        for(let card of cards){
+            this.trelloService.trello.get('/cards/' + card.id + '/members', (members) => this.addMemberPoints(members, card));
         }
     }
 
-    printCardNames(cards){
-        for(let card of cards){
-            console.log(card.name);
+    addMemberPoints(members, card){
+        for(let member of members){
+            var currMember = this.findMember(member.fullName);
+            this.pieChartData[currMember] += Number(card.name.substring(1,2));
+        }
+    }
+
+    findMember(name): number{
+        for(var i = 0; i < this.pieChartLabels.length; i++){
+            if(this.pieChartLabels[i] == name){
+                return i;
+            }
+        }
+    }
+
+    bargraphLists(lists){
+        var sprintCompleteLists = [];
+        for(let list of lists){
+            if(list.name.includes("Sprint") && list.name.includes("Complete")){
+                sprintCompleteLists.push(list);
+            }
+        }
+
+        for(let list of sprintCompleteLists){
+            var pointCount: number = 0;
+            this.barChartLabels.push(list.name);
+            for(let card of list.cards){
+                console.log(card.name);
+                pointCount += Number(card.name.substring(1,2));
+            }
+            this.barChartData[0].data.push(pointCount);
         }
     }
 }
